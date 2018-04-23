f4cdbd461e3a6b59ef586c9a6b8ba67586ac6735
piersrueb <piersrueb@gmail.com>
2018-04-23 11:21:07 +0100

    init

---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+//  requires
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const webserver = require('gulp-webserver');
+const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
+const minify = require('gulp-minify');
+const autoprefixer = require('gulp-autoprefixer');
+const tinypng = require('gulp-tinypng-compress');
+const modernizr = require('gulp-modernizr');
+const cssbeautify = require('gulp-cssbeautify');
+
+//  sass and vendor prefixes
+
+const autoprefixerOptions = {
+    browsers: [
+        'last 2 version',
+        'safari 5',
+        'ie 7', 'ie 8', 'ie 9',
+        'opera 12.1',
+        'ios 6', 'android 4'
+    ]
+};
+
+gulp.task('styles', function() {
+    gulp.src('css/sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer(autoprefixerOptions))
+        .pipe(cssbeautify())
+        .pipe(gulp.dest('./css/'))
+});
+
+//  modernizr
+
+gulp.task('modernizr', function() {
+    gulp.src('./scripts/maximised/*.js')
+        .pipe(modernizr({
+            options: [
+                "setClasses",
+        		"addTest",
+        		"html5printshiv",
+        		"testProp",
+        		"fnBind"
+            ]
+        }))
+        .pipe(gulp.dest("./scripts/maximised/"))
+});
+
+//  concat js files
+
+gulp.task('scripts', function() {
+    return gulp.src('./scripts/maximised/*.js')
+        .pipe(concat('./scripts.js'))
+        .pipe(gulp.dest('./scripts/'));
+});
+
+//  tidy CSS
+
+gulp.task('tidy', function() {
+    return gulp.src('./css/*.css')
+        .pipe(cssbeautify())
+        .pipe(gulp.dest('./css/'));
+});
+
+//  webserver - launch localhost
+
+gulp.task('webserver', function() {
+    gulp.src('')
+        .pipe(webserver({
+            livereload: true,
+            directoryListing: true,
+            open: true
+    }));
+});
+
+//  run tasks + watch
+
+gulp.task('default', ['webserver', 'watch']);
+gulp.task('watch', function() {
+    gulp.watch('./scripts/maximised/*.js', ['scripts']);
+    gulp.watch('css/sass/*.scss',['styles']);
+})
